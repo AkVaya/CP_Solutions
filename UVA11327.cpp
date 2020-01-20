5fc7ee48f3b9d3613754d6df1b1fdd0a3cc0239d
AkVaya <akshat.18je0072@am.iitism.ac.in>
2020-01-20 16:32:37 +0530

    UVA

---
+++ UVA11327.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<math.h>
+#include<algorithm>
+#include<stdlib.h>
+#include<stack>
+#include<set>
+#include<string.h>
+#include<map>
+#include<vector>
+#define pb push_back
+#define ins insert 
+#define mp make_pair
+#define ll long long 
+#define max3(a,b,c) max(c,max(a,b))
+#define min3(a,b,c) min(c,min(a,b))
+#define M 10000007
+using namespace std;
+ll phi(ll n) {
+    ll result = n;
+    for (ll i = 2; i * i <= n; i++) {
+        if(n % i == 0) {
+            while(n % i == 0)
+                n /= i;
+            result -= result / i;
+        }
+    }
+    if(n > 1)
+        result -= result / n;
+    return result;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll a;
+    while(1){
+    	cin>>a;
+    	if(!a)
+    		break;
+    	cout<<phi(a)<<endl;
+    }
+    return 0;
+}
